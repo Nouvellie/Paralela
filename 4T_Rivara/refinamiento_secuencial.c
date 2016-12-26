95beaa90eb88410988758fdf2c90418724d0e261
Nouvellie <remains_of_hate@hotmail.com>
2016-12-26 06:41:11 -0300

    refinamiento_secuencial.c updated

--- 4T_Rivara/refinamiento_secuencial.c
+++ 4T_Rivara/refinamiento_secuencial.c
@@ -1,9 +1,9 @@
+
 #include <stdio.h>
 //#include <metis.h>
 #include <math.h> 
 #include <estructuras.h>
 #include <metodos.h>
-//posible error		#include <graphics.h> 
 
 typedef struct triangulo t;
 //Calculamos los angulos, segun los lados del triangulo
@@ -19,6 +19,5 @@ void refinamiento_triangulacion(t t1,t malla[]){
 }
 int main(){
 	system("clear");
-//posible error		line ( 1, 2, 3, 4 );
 	return 0;
 }
